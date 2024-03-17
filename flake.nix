f6c957a86e181d33cca06981193f2c15a04965e5
Igor Ramazanov <igor.ramazanov@magine.com>
2024-03-17 17:36:10 +0000

    Add home-manager module
    Provides an option:
    ```nix
    programs.kak-tree-sitter-helix.enable = true;
    ```

--- flake.nix
+++ flake.nix
@@ -45,7 +45,6 @@
             rev = "9fccddd14b66a63c2c93d4069fee6a51155e5a9e";
             hash = "sha256-0xF0i+f1s5wdW/GfeuudaVSlSx74y5zLOV/5rP0UD8E=";
           };
-          patches = [./9fccddd14b66a63c2c93d4069fee6a51155e5a9e.patch];
           cargoLock = {
             lockFile = "${src}/Cargo.lock";
           };
@@ -53,21 +52,15 @@
           cargoBuildFlags = ["--package=kak-tree-sitter"];
           nativeBuildInputs = [git];
         };
-      package = pkgs.callPackage kak-tree-sitter {};
-      grammars = pkgs.callPackage (import ./gen-grammars.nix {inherit helix;}) {};
-      final = pkgs.runCommandLocal "kak-tree-sitter-full" {} ''
-        mkdir -p $out/share/kak-tree-sitter
-
-        cp --dereference --recursive ${grammars}/grammars  $out/share/kak-tree-sitter/grammars
-        cp --dereference --recursive ${grammars}/queries   $out/share/kak-tree-sitter/queries
-        cp --dereference --recursive ${package}/bin        $out/bin
-      '';
+      kts-package = pkgs.callPackage kak-tree-sitter {};
+      kts-config = pkgs.callPackage (import ./gen-config.nix {inherit helix;}) {};
+      kts-grammars = pkgs.callPackage (import ./gen-grammars.nix {inherit helix;}) {};
     in {
       formatter = pkgs.alejandra;
-      packages.default = final;
+      packages.default = kts-package;
       apps.default = {
         type = "app";
-        program = "${final}/bin/kak-tree-sitter";
+        program = "${kts-package}/bin/kak-tree-sitter";
       };
       devShell = pkgs.devshell.mkShell {
         name = "kak-tree-sitter-helix";
@@ -77,6 +70,19 @@
           python = [(pkgs.python311.withPackages (p: [p.flake8 p.python-lsp-server p.python-lsp-black]))];
         in
           rust ++ python;
+      };
+      homeManagerModules.kak-tree-sitter-helix = {
+        config,
+        lib,
+        ...
+      }: {
+        options.programs.kak-tree-sitter-helix.enable = lib.options.mkEnableOption "Enable kak-tree-sitter-helix";
+        config = lib.mkIf config.programs.kak-tree-sitter-helix.enable {
+          home.packages = [kts-package];
+          xdg.configFile."kak-tree-sitter/config.toml".source = "${kts-config}/config.toml";
+          xdg.dataFile."kak-tree-sitter/grammars".source = "${kts-grammars}/grammars";
+          xdg.dataFile."kak-tree-sitter/queries".source = "${kts-grammars}/queries";
+        };
       };
     });
 }
