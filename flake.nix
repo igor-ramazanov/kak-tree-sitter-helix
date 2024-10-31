df3ccd0a3bc37a9812aa95f556c00bc8aff3f81a
Igor Ramazanov <igor.ramazanov@magine.com>
2024-10-31 17:11:24 +0000

    Patch `kts` to read the `name` field from the generated config

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
             rev = "955b31df81532fc67a2e56279457d587f1d615fb";
             hash = "sha256-LuFI0tRtZZ9C6xKRUbSzE/oI8Z7djo7RvF5xM8776gI=";
           };
+          patches = [./kts.patch];
           cargoLock = {lockFile = "${src}/Cargo.lock";};
           # We only need to build kak-tree-sitter, no need to waste time on ktsctl.
           cargoBuildFlags = ["--package=kak-tree-sitter"];
