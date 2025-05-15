8f06027a3769010d8072c370065f954f6556f83a
Igor Ramazanov <igor.ramazanov@magine.com>
2025-05-15 23:18:01 +0000

    Provide nixos module, refactor nix code

---
+++ nix/kak-tree-sitter-package.nix
@@ -0,0 +1,31 @@
+{
+  fetchFromGitHub,
+  git,
+  rustPlatform,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "kak-tree-sitter";
+
+  version = let
+    cargoTomlPath = "${src}/kak-tree-sitter/Cargo.toml";
+    cargoTomlContent = builtins.readFile cargoTomlPath;
+    cargoToml = builtins.fromTOML cargoTomlContent;
+  in
+    cargoToml.package.version;
+
+  src = fetchFromGitHub {
+    owner = "hadronized";
+    repo = "kak-tree-sitter";
+    rev = "955b31df81532fc67a2e56279457d587f1d615fb";
+    hash = "sha256-LuFI0tRtZZ9C6xKRUbSzE/oI8Z7djo7RvF5xM8776gI=";
+  };
+
+  nativeBuildInputs = [git];
+
+  patches = [./kts.patch];
+
+  cargoLock = {lockFile = "${src}/Cargo.lock";};
+
+  # We only need to build kak-tree-sitter, no need to waste time on ktsctl.
+  cargoBuildFlags = ["--package=kak-tree-sitter"];
+}
